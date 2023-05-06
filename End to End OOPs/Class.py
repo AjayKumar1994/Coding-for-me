a5793db57f5eeea01af8648eb57473028de238c3
Ajay <akumar92mail@gmail.com>
2023-05-06 22:01:44 +0530

    Opps

---
+++ End to End OOPs/Class.py
@@ -0,0 +1,13 @@
+### Class Basic Structure
+### Class name should be in Pascal case i.e. ThisIsPascalCase
+### Camel case: thisIsCamelCase
+### Snake case: snake_case 
+
+
+class Car:
+    # data or variable name should be in snake case
+    color = "blue" # data
+    model = "sports" # data
+
+    def calculate_avg_speed(km, time):
+        # Some code
